da1cf36dec5e7df4c359706b1bd30b80e19ca368
Roy Portas <royportas@gmail.com>
2018-07-02 20:59:28 +1000

    Added files

---
+++ Button/Button.c
@@ -0,0 +1,12 @@
+#include "Arduino.h"
+#include "Button.h"
+
+/**
+ * This stores the implementation of the libary
+ *
+ * If your looking at how to use it, check out Button.h instead
+ */
+
+Button::Button(int pin) {
+    // Lets set up the pins
+}
---
+++ Button/Button.h
@@ -0,0 +1,37 @@
+/**
+ * A basic library to make using Buttons easier
+ *
+ * @author Roy Portas
+ */
+#ifndef Button_h
+#define Button_h
+
+#include "Arduino.h"
+
+class Button {
+    public:
+
+        /**
+         * Creates a button
+         *
+         * Specify the number of the pin to use
+         */
+        Button(int pin);
+
+        /**
+         * Returns `true` if the button is pressed
+         * otherwise `false`
+         */
+        bool isPressed();
+
+    /**
+     * These are used within the library
+     *
+     * Reading further isn't required,
+     * unless your interested
+     */
+    private:
+        int _pin;
+};
+
+#endif
